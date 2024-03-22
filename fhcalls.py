4b5d036542f88ca3aa1f2aacf03de1606689effd
Kamalpreet Singh <136398706+kannukamal@users.noreply.github.com>
2024-03-22 14:02:44 +0530

    Update fhcalls.py

--- fhcalls.py
+++ fhcalls.py
@@ -29,37 +29,37 @@ def generate_url(url_m):
         print("URL where we can get more details related to PLP like Facets, Sortings, etc. : ", str1)
 
         print("str1 : ", str1)
-        fh_url_to_be_changed = get_api_response(str1)
-        print("fh_url_to_be_changed : ", fh_url_to_be_changed)
-        print("-------------------------------STAGING OUTPUT------------------------------------")
-        fh_preview_url_stg = fh_url_to_be_changed.replace("fredhopper/query", "preview")
-        print("Fredhopper Business Manager Link with all Modifications for STG : ", fh_preview_url_stg)
+#         fh_url_to_be_changed = get_api_response(str1)
+#         print("fh_url_to_be_changed : ", fh_url_to_be_changed)
+#         print("-------------------------------STAGING OUTPUT------------------------------------")
+#         fh_preview_url_stg = fh_url_to_be_changed.replace("fredhopper/query", "preview")
+#         print("Fredhopper Business Manager Link with all Modifications for STG : ", fh_preview_url_stg)
 
-        fh_preview_url_wm_stg = fh_preview_url_stg + "&fh_suppress=modifications:all"
-        print("Fredhopper Business Manager Link without Modifications for STG : ", fh_preview_url_wm_stg)
+#         fh_preview_url_wm_stg = fh_preview_url_stg + "&fh_suppress=modifications:all"
+#         print("Fredhopper Business Manager Link without Modifications for STG : ", fh_preview_url_wm_stg)
 
-        print("-------------------------------PRODUCTION OUTPUT------------------------------------------")
+#         print("-------------------------------PRODUCTION OUTPUT------------------------------------------")
 
-        fh_preview_url_prod = fh_preview_url_stg.replace(".prepublished", ".published")
-        print("Fredhopper Business Manager Link with all Modifications for PROD : ", fh_preview_url_prod)
+#         fh_preview_url_prod = fh_preview_url_stg.replace(".prepublished", ".published")
+#         print("Fredhopper Business Manager Link with all Modifications for PROD : ", fh_preview_url_prod)
 
-        fh_preview_url_wm_prod = fh_preview_url_wm_stg.replace(".prepublished", ".published")
-        print("Fredhopper Business Manager Link without Modifications for PROD : ", fh_preview_url_wm_prod)
-        print("-------------------------------------------------------------------------")
-def get_api_response(api_url):
-    try:
-        response = requests.get(api_url, verify=False)
-        response.raise_for_status()
-        print("Response status code is", response.status_code)
-        if response.status_code == 200:
-            data = response.json()
-            print("Number of articles currently available on PLP as per the Fredhopper results are : ", data['_debug']['productCount'])
-            print("The URL printed for debugging purpose :", data['raw']['debug']['url'])
-            return data['raw']['debug']['url']
-        else:
-            print(f"An error occurred : {response.status_code}")
-    except Exception as e:
-        print(f"An error occurred : {str(e)}")
+#         fh_preview_url_wm_prod = fh_preview_url_wm_stg.replace(".prepublished", ".published")
+#         print("Fredhopper Business Manager Link without Modifications for PROD : ", fh_preview_url_wm_prod)
+#         print("-------------------------------------------------------------------------")
+# def get_api_response(api_url):
+#     try:
+#         response = requests.get(api_url, verify=False)
+#         response.raise_for_status()
+#         print("Response status code is", response.status_code)
+#         if response.status_code == 200:
+#             data = response.json()
+#             print("Number of articles currently available on PLP as per the Fredhopper results are : ", data['_debug']['productCount'])
+#             print("The URL printed for debugging purpose :", data['raw']['debug']['url'])
+#             return data['raw']['debug']['url']
+#         else:
+#             print(f"An error occurred : {response.status_code}")
+#     except Exception as e:
+#         print(f"An error occurred : {str(e)}")
 print("Python file execution started")
 generate_url("https://www.adidas.com.sg/women-shoes-new_arrivals")
 print(generate_url("https://www.adidas.com.sg/women-shoes-new_arrivals"))
