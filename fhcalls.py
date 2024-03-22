ecd36c5730ea8f2b68b5f58c05a5d448ef6e11ba
Kamalpreet Singh <136398706+kannukamal@users.noreply.github.com>
2024-03-22 13:53:49 +0530

    Update fhcalls.py

--- fhcalls.py
+++ fhcalls.py
@@ -1,3 +1,4 @@
+from urllib.parse import urlparse
 def generate_url(url_m):
     print("PLP URL entered :", url_m)
     if "staging" not in url_m:
@@ -10,7 +11,9 @@ def generate_url(url_m):
 
         url_m2 = str2 + '&debug=true'
         print(url_m2)
+        str1 = urlparse(url_m2).scheme + "://" + urlparse(url_m2).netloc + "/api/plp/content-engine?&query=" + urlparse(url_m2).path.replace('/', '', 1)
 
+        print("URL where we can get more details related to PLP like Facets, Sortings, etc. : ", str1)
 print("Python file execution started")
 generate_url("https://www.adidas.com.sg/women-shoes-new_arrivals")
 print(generate_url("https://www.adidas.com.sg/women-shoes-new_arrivals"))
